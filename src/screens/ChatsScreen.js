81340416150d616457df98f5d4d675c21c388743
dloyolar <dieegolr@gmail.com>
2022-12-05 21:17:19 -0300

    refactor: remove unused SafeAreaView

--- src/screens/ChatsScreen.js
+++ src/screens/ChatsScreen.js
@@ -1,17 +1,15 @@
-import { View, Text, FlatList, SafeAreaView } from 'react-native';
+import { FlatList } from 'react-native';
 import React from 'react';
 import chats from '../../assets/data/chats.json';
 import { ChatListItem } from '../components/ChatListItem';
 
 const ChatsScreen = () => {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <FlatList
-        data={chats}
-        renderItem={({ item }) => <ChatListItem chat={item} />}
-        style={{ backgroundColor: 'white' }}
-      />
-    </SafeAreaView>
+    <FlatList
+      data={chats}
+      renderItem={({ item }) => <ChatListItem chat={item} />}
+      style={{ backgroundColor: 'white' }}
+    />
   );
 };
 
