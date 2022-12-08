9c60d09f6fe2b30b0db76cc05297e84735318439
dloyolar <dieegolr@gmail.com>
2022-12-08 03:16:58 -0300

    refactor: validate input in chat screen and msg

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,24 +1,47 @@
-import { useState } from 'react';
 import { Keyboard } from 'react-native';
 import { IconButton } from 'react-native-paper';
+import { useForm, Controller } from 'react-hook-form';
 import { Container, Input, SendButton } from './ui';
 
-export const InputBox = () => {
-  const [newMessage, setNewMessage] = useState('');
+export const InputBox = ({ onNewMessage }) => {
+  const { control, handleSubmit, formState, reset } = useForm();
 
-  const onSend = () => {
-    console.warn('Sending new message: ', newMessage);
+  const onSend = handleSubmit((data) => {
+    const newMessage = data.newMessage.trim();
+    onNewMessage(newMessage);
     Keyboard.dismiss();
-    setNewMessage('');
-  };
+    reset();
+  });
 
   return (
     <Container>
       <IconButton icon="plus" color="royalblue" size={24} onPress={() => console.log('Pressed')} />
 
-      <Input placeholder="Escribe un mensaje..." value={newMessage} onChangeText={setNewMessage} />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+          maxLength: 200,
+          validate: (value) => !!value.trim(),
+        }}
+        name="newMessage"
+        render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
+          <Input
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Escribe un mensaje..."
+          />
+        )}
+      />
 
-      <SendButton onPress={onSend} icon="send" color="white" size={24} />
+      <SendButton
+        onPress={onSend}
+        icon="send"
+        color="white"
+        size={24}
+        disabled={!formState.isValid}
+      />
     </Container>
   );
 };
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -5,7 +5,7 @@ import messages from '../../assets/data/messages.json';
 import bg from '../../assets/images/wsp-bg.png';
 import { Message } from '../components/Message';
 import { InputBox } from '../components/InputBox';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ImageBG = styled(ImageBackground)`
   flex: 1;
@@ -15,25 +15,54 @@ const ChatScreen = () => {
   const navigation = useNavigation();
   const route = useRoute();
 
+  const [allMessages, setAllMessages] = useState([]);
+
   useEffect(() => {
     navigation.setOptions({ title: route.params.name });
   }, [route.params]);
 
+  useEffect(() => {
+    sortMessages();
+  }, []);
+
+  const sortMessages = () => {
+    const msgs = messages.sort((a, b) => {
+      return a.createdAt < b.createdAt ? 1 : a.createdAt > b.createdAt ? -1 : 0;
+    });
+    setAllMessages([...msgs]);
+  };
+
+  const onNewMessage = (message) => {
+    console.log({ message });
+    const tempMessage = {
+      id: Date.now(),
+      text: message,
+      createdAt: new Date().toISOString(),
+      user: {
+        id: 'u1',
+        name: 'Vadim',
+      },
+    };
+
+    setAllMessages([tempMessage, ...allMessages]);
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 90}
+      keyboardVerticalOffset={90}
       style={{ flex: 1 }}
     >
       <ImageBG source={bg}>
         <FlatList
-          data={messages}
+          data={allMessages}
           renderItem={({ item }) => <Message message={item} />}
           style={{ padding: 10 }}
           inverted
+          extraData={allMessages}
         />
-        <InputBox />
       </ImageBG>
+      <InputBox onNewMessage={onNewMessage} />
     </KeyboardAvoidingView>
   );
 };
