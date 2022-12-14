ea786b0b59d8caed176e055255e7a2a0e41a087e
dloyolar <dieegolr@gmail.com>
2022-12-14 17:11:07 -0300

    refactor: get chats from backend

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -17,18 +17,20 @@ export const ChatListItem = ({ chat }) => {
   const navigation = useNavigation();
 
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate('Chat', { id: chat.id, name: chat.user.name })}
-    >
+    <TouchableOpacity onPress={() => navigation.navigate('Chat', { id: chat.id, name: chat.name })}>
       <StyledListItem
-        title={chat.user.name}
+        title={chat.name}
         titleStyle={{ fontWeight: 'bold' }}
-        description={chat.lastMessage.text}
+        description={chat.lastMessage || ''}
         descriptionNumberOfLines={1}
         descriptionEllipsizeMode="tail"
-        left={() => <ChatListImage imageUrl={chat.user.image} />}
+        left={() => (
+          <ChatListImage imageUrl={chat.image || 'https://freesvg.org/img/Female-Avatar-5.png'} />
+        )}
         right={() => (
-          <Text style={{ color: 'gray' }}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
+          <Text style={{ color: 'gray' }}>
+            {dayjs(chat?.lastMessage?.createdAt).fromNow(true) || ''}
+          </Text>
         )}
       />
     </TouchableOpacity>
--- src/screens/ChatsScreen.js
+++ src/screens/ChatsScreen.js
@@ -1,9 +1,37 @@
 import { FlatList } from 'react-native';
-import React from 'react';
-import chats from '../../assets/data/chats.json';
+import React, { useContext, useEffect, useState } from 'react';
 import { ChatListItem } from '../components/ChatListItem';
+import { AuthContext } from '../context/AuthProvider';
+import { collection, getDocs, onSnapshot, query, where } from 'firebase/firestore';
+import { firestore } from '../services/firebase';
 
 const ChatsScreen = () => {
+  const { auth } = useContext(AuthContext);
+  const [chats, setChats] = useState([]);
+  console.log({ auth });
+
+  useEffect(() => {
+    const unsuscribe = onSnapshot(
+      query(collection(firestore, '/chats'), where('phones', 'array-contains', auth)),
+      (snapshot) => {
+        const firestoreChats = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            name: data.phones[0] === auth ? data.phones[1] : data.phones[0],
+            lastMessage: data.lastMessage,
+          };
+        });
+        console.log({ firestoreChats });
+        setChats(firestoreChats);
+      }
+    );
+
+    return () => {
+      unsuscribe();
+    };
+  }, [auth]);
+
   return (
     <FlatList
       data={chats}
