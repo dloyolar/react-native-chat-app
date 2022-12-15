f74c2fa44499b7450f294521c30933b8c424930f
dloyolar <dieegolr@gmail.com>
2022-12-14 21:35:20 -0300

    chore: add expo contacts

---
+++ src/hooks/useContacts.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import * as Contacts from 'expo-contacts';
+
+export const useContacts = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Contacts.requestPermissionsAsync();
+      if (status === 'granted') {
+        const { data } = await Contacts.getContactsAsync({
+          fields: [
+            Contacts.Fields.FirstName,
+            Contacts.Fields.LastName,
+            Contacts.Fields.PhoneNumbers,
+          ],
+        });
+
+        if (data.length > 0) {
+          setContacts(data);
+        }
+      }
+    })();
+  }, []);
+
+  return contacts;
+};
