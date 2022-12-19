2c1c20d6c5b874d8eb57f965a614e9b40fce64a6
dloyolar <dieegolr@gmail.com>
2022-12-19 20:53:59 -0300

    chore: add expo img picker

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -29,3 +30,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore();
+export const storage = getStorage();
