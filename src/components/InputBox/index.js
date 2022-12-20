44894d69949605abbf1362f2e535e21dd6540f9b
dloyolar <dieegolr@gmail.com>
2022-12-19 21:01:32 -0300

    refactor: create util functions to upload imgs

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -2,37 +2,7 @@ import { Keyboard } from 'react-native';
 import { IconButton } from 'react-native-paper';
 import { useForm, Controller } from 'react-hook-form';
 import { Container, Input, SendButton } from './ui';
-import * as ImagePicker from 'expo-image-picker';
-import { ref, uploadBytes } from 'firebase/storage';
-import { firestore, storage } from '../../services/firebase';
-import { addDoc, collection } from 'firebase/firestore';
-
-const uploadImage = async (uri, auth, chatId) => {
-  const extension = uri.split('.').slice(-1)[0];
-  const path = 'chats/' + chatId + '/' + new Date().toISOString() + '.' + extension;
-  const imageRef = ref(storage, path);
-  const blob = await new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-      resolve(xhr.response);
-    };
-    xhr.onerror = function (e) {
-      console.log(e);
-      reject(new TypeError('Network request failed'));
-    };
-    xhr.responseType = 'blob';
-    xhr.open('GET', uri, true);
-    xhr.send(null);
-  });
-  await uploadBytes(imageRef, blob);
-
-  addDoc(collection(firestore, 'chats', chatId, 'messages'), {
-    content: path,
-    createdAt: new Date(),
-    phone: auth,
-    type: 'image',
-  });
-};
+import { pickImage, takePhoto } from '../../utils/uploadImage';
 
 export const InputBox = ({ onNewMessage, auth, chatId }) => {
   const { control, handleSubmit, formState, reset } = useForm();
@@ -44,44 +14,14 @@ export const InputBox = ({ onNewMessage, auth, chatId }) => {
     reset();
   });
 
-  const pickImage = async () => {
-    const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!permission.granted) {
-      Alert.alert('Necesitamos permiso para acceder a tus imágenes');
-      return;
-    }
-
-    const media = await ImagePicker.launchImageLibraryAsync();
-
-    if (media.canceled) {
-      console.log('cancelled');
-      return;
-    }
-
-    console.log(media.assets[0].uri);
-    console.log({ auth });
-    console.log({ chatId });
-
-    await uploadImage(media.assets[0].uri, auth, chatId);
-  };
-
-  const takePhoto = async () => {
-    const permission = await ImagePicker.requestCameraPermissionsAsync();
-    if (!permission.granted) {
-      Alert.alert('Necesitamos los permisos');
-      return;
-    }
-
-    const media = await ImagePicker.launchCameraAsync();
-    if (media.canceled) {
-      return;
-    }
-    await uploadImage(media.assets[0].uri, auth, chatId);
-  };
-
   return (
     <Container>
-      <IconButton icon="attachment" color="royalblue" size={24} onPress={pickImage} />
+      <IconButton
+        icon="attachment"
+        color="royalblue"
+        size={24}
+        onPress={() => pickImage(auth, chatId)}
+      />
 
       <Controller
         control={control}
@@ -101,7 +41,12 @@ export const InputBox = ({ onNewMessage, auth, chatId }) => {
         )}
       />
 
-      <IconButton icon="camera" color="royalblue" size={24} onPress={takePhoto} />
+      <IconButton
+        icon="camera"
+        color="royalblue"
+        size={24}
+        onPress={() => takePhoto(auth, chatId)}
+      />
       <SendButton
         onPress={onSend}
         icon="send"
---
+++ src/utils/uploadImage.js
@@ -0,0 +1,66 @@
+import { addDoc, collection } from 'firebase/firestore';
+import { firestore, storage } from '../services/firebase';
+import { ref, uploadBytes } from 'firebase/storage';
+import * as ImagePicker from 'expo-image-picker';
+
+export const uploadImage = async (uri, auth, chatId) => {
+  const extension = uri.split('.').slice(-1)[0];
+  const path = 'chats/' + chatId + '/' + new Date().toISOString() + '.' + extension;
+  const imageRef = ref(storage, path);
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function (e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+  await uploadBytes(imageRef, blob);
+
+  addDoc(collection(firestore, 'chats', chatId, 'messages'), {
+    content: path,
+    createdAt: new Date(),
+    phone: auth,
+    type: 'image',
+  });
+};
+
+export const pickImage = async (auth, chatId) => {
+  const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  if (!permission.granted) {
+    Alert.alert('Necesitamos permiso para acceder a tus imágenes');
+    return;
+  }
+
+  const media = await ImagePicker.launchImageLibraryAsync();
+
+  if (media.canceled) {
+    console.log('cancelled');
+    return;
+  }
+
+  console.log(media.assets[0].uri);
+  console.log({ auth });
+  console.log({ chatId });
+
+  await uploadImage(media.assets[0].uri, auth, chatId);
+};
+
+export const takePhoto = async (auth, chatId) => {
+  const permission = await ImagePicker.requestCameraPermissionsAsync();
+  if (!permission.granted) {
+    Alert.alert('Necesitamos los permisos');
+    return;
+  }
+
+  const media = await ImagePicker.launchCameraAsync();
+  if (media.canceled) {
+    return;
+  }
+  await uploadImage(media.assets[0].uri, auth, chatId);
+};
