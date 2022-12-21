d1d1a4536de5265c06d79f4ab6a4316f3d20230a
dloyolar <dieegolr@gmail.com>
2022-12-21 18:37:28 -0300

    feat: add persist phone in storage

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,13 +1,32 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as SplashScreen from 'expo-splash-screen';
 
 export const AuthContext = createContext();
+
+const AUTH_KEY = 'phone';
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState('');
 
-  const login = (phone) => setAuth(phone);
+  useEffect(function loadFromAsyncStorage() {
+    AsyncStorage.getItem(AUTH_KEY).then((value) => {
+      if (value) {
+        setAuth(value);
+      }
+      SplashScreen.hideAsync();
+    });
+  }, []);
 
-  const logout = () => setAuth('');
+  const login = (phone) => {
+    setAuth(phone);
+    void AsyncStorage.setItem(AUTH_KEY, phone);
+  };
+
+  const logout = () => {
+    setAuth('');
+    void AsyncStorage.setItem(AUTH_KEY, '');
+  };
 
   return <AuthContext.Provider value={{ auth, login, logout }}>{children}</AuthContext.Provider>;
 };
